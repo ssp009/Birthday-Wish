22b3adc987b8abb28ea7479a3afc6a79d1147d30
ssp009 <113298019+ssp009@users.noreply.github.com>
2022-09-11 20:43:04 +0600

    Birthday wish

---
+++ index.js
@@ -0,0 +1,25 @@
+
+document.querySelector("button").addEventListener("click", myFunction);
+
+function myFunction() {
+   var names =  document.getElementById("name").value;
+      names = names.toUpperCase();
+   if (names==null || names==""){  
+      alert("Invalid Name");  
+      return false; }
+   var dates = document.getElementById("date").value;
+   
+   var day = new Date(dates);
+   
+   
+   var today = new Date();
+   
+   
+   
+   if(day.getDay()== today.getDay() && day.getMonth() == today.getMonth() ){
+      return alert("Happy Birthday! " +  names + " Hope almighty Allah gives you all the happines and prosperity in life" )
+   } else {
+      return alert("Have to a good day!")
+   }  
+}
+
